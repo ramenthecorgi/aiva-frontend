3f83fed116ab0e10ba81d2c3cfb9d104d70a26be
yuf <yuefan011@gmail.com>
2025-07-27 18:13:34 -0700

    fixing deeplink issue within page. disable login flow in prod

--- src/config/featureFlags.js
+++ src/config/featureFlags.js
@@ -2,6 +2,7 @@
 const featureFlags = {
   // Set this to true to enable login functionality
   enableLogin: process.env.REACT_APP_ENABLE_LOGIN === 'true',
+  enableOnboarding: process.env.REACT_APP_ENABLE_ONBOARDING === 'true',
 };
 
 export default featureFlags;
